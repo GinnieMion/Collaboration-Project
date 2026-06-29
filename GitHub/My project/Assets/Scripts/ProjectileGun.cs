763da37b71bd7d1a41c473a69917d619ec4f26f7
aidanwhite148-gif <aidan.white.148@accesscreative.ac.uk>
2026-06-29 11:12:45 +0100

    gun projectile

--- GitHub/My project/Assets/Scripts/ProjectileGun.cs
+++ GitHub/My project/Assets/Scripts/ProjectileGun.cs
@@ -5,5 +5,110 @@ public class ProjectileGun : MonoBehaviour
     public GameObject bullet;
 
     public float shootForce, upwardForce;
-     m
+
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+
+    bool shooting, readyToShoot, reloading;
+
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        if (ammunitionDisplay != null)
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+    }
+    private void MyInput()
+    {
+        if(allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKey(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        if (readyToShoot && shooting & !reloading && bulletsLeft > 0) Reload();
+
+        if(readyToShoot && shooting && bulletsLeft > 0)
+        {
+            bulletsShot = 0;
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 directionWithSperead = directionWithoutSpread + new Vector3(x, y, 0);
+
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        currentBullet.transform.forward = directionWithoutSpread.normalized;
+
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSperead.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        if (muzzleFlash != null)
+            Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        bulletsLeft--;
+        bulletsShot++;
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);  
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("Reloadfinished", reloadTime);
+    }
+
+    private  void ReloadFinshed()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
 }
